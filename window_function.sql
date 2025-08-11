28d8b8ab4970bf035611fa4b3f5a1b6182783339
Chisom Nwokwu <ceekay2a7@gmail.com>
2025-08-11 12:38:55 +0100

    Update window_function.sql

--- window_function.sql
+++ window_function.sql
@@ -16,3 +16,26 @@ employee, region, sales,
 DENSE_RANK() OVER (PARTITION BY region ORDER BY sales DESC) AS rank
 FROM sales;
 
+--AGGREGATE FUNCTIONS
+SELECT
+employee,
+region,
+sales,
+SUM(sales) OVER (PARTITION BY region) AS total_sales_region,
+AVG(sales) OVER (PARTITION BY region) AS avg_sales_region
+FROM sales;
+
+--LAG
+SELECT
+month,
+sales,
+LAG(sales) OVER (ORDER BY month) AS prev_month_sales
+FROM sales;
+
+-LEAD
+SELECT
+month,
+sales,
+LEAD(sales) OVER (ORDER BY month) AS next_month_sales
+FROM sales;
+
